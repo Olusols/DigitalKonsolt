3189ea299a9735fc5167aa47d58bd455b373240a
Olusola <81810711+Olusols@users.noreply.github.com>
2022-05-26 12:19:18 +0100

    changed because of collect static

--- DigitalKonsolt/settings.py
+++ DigitalKonsolt/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
     
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    #'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'DigitalKonsolt.urls'
